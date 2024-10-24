904d8d8bc7a323dd13805730c8b5fc571615eba6
Robert Pliszka <robplisz@gmail.com>
2024-10-24 19:12:42 -0400

    Finished getting information about review being displayed

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import reviews from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa6';
 
 const App = () => {
-  const [index, setIndex] = useState(0
-    
-  );
+  const [index, setIndex] = useState(0);
 
   const { id, name, job, image, text } = reviews[index];
 
@@ -15,7 +13,13 @@ const App = () => {
       <article className="review">
         <div className="img-container">
           <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
         </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
       </article>
     </main>
   );
